2317919b692e751881c58afc958b5e306676482e
daniel raya <danielraya316@outlook.com>
2016-10-23 00:23:21 -0500

    initial demo progect

---
+++ example/src/main/java/com/noomrs/dan/example/DanApplication.java
@@ -0,0 +1,12 @@
+package com.noomrs.dan.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanApplication.class, args);
+	}
+}
